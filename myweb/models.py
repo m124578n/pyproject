9ccde98915b7d8b4e09f1e8b36271a9d1bcb6789
m124578n <m23568n@gmail.com>
2022-04-26 16:45:33 +0800

    main

--- myweb/models.py
+++ myweb/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Test(models.Model):
+    content = models.CharField(max_length=200)
+    visitor = models.CharField(max_length=20)
+    email = models.EmailField(max_length=20)
+    date_time = models.DateTimeField()
+    
